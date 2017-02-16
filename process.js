886f41dd9a97b0386f433870eb6e5cb8d1706e92
Nathan Hood <nathanhood@me.com>
2017-02-16 11:15:38 -0600

    Add postcss-nested for processing

--- process.js
+++ process.js
@@ -2,15 +2,17 @@ const postcss = require('postcss');
 const fs = require('fs');
 const variables = require('postcss-variables');
 const mixins = require('postcss-js-mixins');
+const nested = require('postcss-nested');
 const plugins = [
 	variables({
 		globals: require('./variables')
 	}),
 	mixins({
 		mixins: require('./mixins')
-	})
+	}),
+	nested()
 ];
-const css = fs.readFileSync('./styles/wee.reset.pcss');
+const css = fs.readFileSync('./styles/components/buttons.pcss');
 
 postcss(plugins).process(css, {
 		syntax: require('postcss-wee-syntax')
