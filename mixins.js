c5acf4862e690872726a7b9a80566df3195abab8
Jay Taylor <jay@caddis.co>
2017-03-03 15:41:04 -0600

    Update mixin imports

--- mixins.js
+++ mixins.js
@@ -1,7 +1,6 @@
 const decl = require('postcss-js-mixins/lib/declaration');
 const rule = require('postcss-js-mixins/lib/rule');
-const { isObject, isEmpty, isPercentage, isColor, prefix, isNumber, hexToRgba, calcOpacity, isString, unit } = require('postcss-js-mixins/lib/helpers');
-
+const { isObject, isEmpty, isPercentage, isColor, prefix, isNumber, hexToRgba, calcOpacity, isString, unit, toNumber, toPercentage } = require('postcss-js-mixins/lib/helpers');
 
 module.exports = (vars = {}) => {
 	return {
